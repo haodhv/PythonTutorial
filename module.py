1f2b5c154e90e236175282b29406350c46022ce8
Tran Xuan Hao <48816341+haodhv@users.noreply.github.com>
2022-12-17 16:06:18 +0700

    Update module.py

--- module.py
+++ module.py
@@ -3,3 +3,10 @@ def giai_thua(n):
         return 1
     else:
         return giai_thua(n - 1) * n
+
+
+def fibonacci(n):
+    if n <= 2:
+        return 1
+    else:
+        return fibonacci(n - 2) + fibonacci(n-1)
