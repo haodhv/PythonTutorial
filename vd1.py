ec1f33cb52fdd1c71038eeeb4db8aa0fb9a26937
Tran Hao <haotx@vinhuni.edu.vn>
2022-12-17 15:35:31 +0700

    upload files

---
+++ vd1.py
@@ -0,0 +1,15 @@
+import module
+import os
+from datetime import date
+
+os.system('cls')
+days = ["Hai", "Ba", "Tư", "Năm", "Sáu", "Bảy", "Chủ Nhật"]
+
+# x = input("Nhập số nguyên: ")
+# print("Ket qua la: ", module.giai_thua(int(x)))
+date = date.today()
+thu = days[date.weekday()]
+ngay = date.day
+thang = date.month
+nam = date.year
+print("Thu ", thu, " ngày ", ngay, " tháng ", thang, " nam ", nam)
